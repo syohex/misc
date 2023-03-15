77f64ab99f95e2305bf0d27d2de46f6f14d4446d
Shohei YOSHIDA <syohex@gmail.com>
2023-03-15 15:23:21 +0900

    Add regex example

---
+++ csharp/regexp01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+var timeRegex = new Regex(@"^(?:(?<hours>[0-9]+):)?(?<minutes>[0-9]{2}):(?<seconds>[0-9]{2}).(?<frac>[0-9]{3})$");
+string[] input = {
+    "00:09:22.000",
+    "11:22.000",
+};
+
+foreach (var d in input)
+{
+    var m = timeRegex.Match(d);
+    if (m.Success)
+    {
+        int hours = -1;
+        int.TryParse(m.Groups["hours"].Value, out hours);
+        var minutes = int.Parse(m.Groups["minutes"].Value);
+        var seconds = int.Parse(m.Groups["seconds"].Value);
+        var secondsFrac = int.Parse(m.Groups["frac"].Value);
+        Console.WriteLine($"matched: hours='{hours}' minutes='{minutes}', seconds='{seconds}', frac='{secondsFrac}'");
+    }
+    else
+    {
+        Console.WriteLine($"{d} is not matched");
+    }
+}
