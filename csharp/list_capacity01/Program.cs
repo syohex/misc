e86fba0a9caf2fb8380880161a24ac7870ea601f
Shohei YOSHIDA <syohex@gmail.com>
2023-09-06 17:34:09 +0900

    Add capacity sample

---
+++ csharp/list_capacity01/Program.cs
@@ -0,0 +1,8 @@
+﻿var a = new List<int>(10);
+Console.WriteLine($"length={a.Count} capacity={a.Capacity} a={string.Join(',', a)}");
+
+for (var i = 0; i < 10; ++i) {
+    a.Add(i);
+}
+
+Console.WriteLine($"length={a.Count} capacity={a.Capacity} a={string.Join(',', a)}");
