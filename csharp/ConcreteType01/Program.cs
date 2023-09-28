3eb872a92ccd4058730887a1c225bc3501fc50fb
Shohei YOSHIDA <syohex@gmail.com>
2023-09-28 14:58:23 +0900

    Add example to retrive concreate class name from interface

---
+++ csharp/ConcreteType01/Program.cs
@@ -0,0 +1,18 @@
+﻿IFoo f1 = new Foo1();
+IFoo f2 = new Foo2();
+
+Console.WriteLine($"f1 type={f1.GetType().FullName}");
+Console.WriteLine($"f2 type={f2.GetType().FullName}");
+
+interface IFoo
+{
+}
+
+class Foo1 : IFoo
+{
+}
+
+class Foo2 : IFoo
+{
+}
+
