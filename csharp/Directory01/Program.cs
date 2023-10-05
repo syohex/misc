3dbd8b3ab2fa417afc0410a080ae733a18b6cd3c
Shohei YOSHIDA <syohex@gmail.com>
2023-10-05 20:32:02 +0900

    Add Directory samples

---
+++ csharp/Directory01/Program.cs
@@ -0,0 +1,8 @@
+﻿var files = Directory.GetFiles("/usr/local/bin/");
+Console.WriteLine($"files={string.Join("  ", files)}");
+
+var dirs = Directory.GetDirectories("/usr/local");
+Console.WriteLine($"files={string.Join("  ", dirs)}");
+
+var f = Path.GetFileName(dirs[0]);
+Console.WriteLine($"basename={f}");
