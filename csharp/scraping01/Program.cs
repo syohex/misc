166acdd28d9eeec1c8140b36082fedd2adaf0818
Shohei YOSHIDA <syohex@gmail.com>
2025-08-13 00:34:33 +0900

    Add playwright c# sample

---
+++ csharp/scraping01/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main()
+    {
+        const string url = "https://video.dmm.co.jp/av/content/?id=sone00638";
+        using var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+        var context = await browser.NewContextAsync();
+
+        var cookie = new Cookie
+        {
+            Name = "age_check_done",
+            Value = "1",
+            Path = "/",
+            Domain = ".dmm.co.jp",
+            HttpOnly = true,
+            Secure = true,
+        };
+        await context.AddCookiesAsync([cookie]);
+
+        var page = await context.NewPageAsync();
+        await page.GotoAsync(url);
+
+        var title = await page.TitleAsync();
+        Console.WriteLine($"Page Title: {title}");
+
+        var handles = await page.Locator("a").ElementHandlesAsync();
+        foreach (var handle in handles)
+        {
+            var src = await handle.GetAttributeAsync("href");
+            if (src == null) continue;
+
+            if (src.EndsWith("pl.jpg"))
+            {
+                Console.WriteLine($"Found Image: {src}");
+                break;
+            }
+        }
+
+        await context.CloseAsync();
+        await browser.CloseAsync();
+    }
+}
